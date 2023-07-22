6bfe4906605543be68e52b8fab272a4334fa98a4
Fateme <fa.bagherzad@gmail.com>
2023-07-22 13:32:13 -0400

    get data rom API just for dog images

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,49 +1,68 @@
-const images = [
-	{
-		url: "./assets/images/Photo-gallery-1.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-2.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-3.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-4.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-5.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
+// const images = [
+//   {
+//     url: './assets/images/Photo-gallery-1.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-2.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-3.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-4.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-5.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
 
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-	{
-		url: "./assets/images/Photo-gallery-6.jpg",
-	},
-];
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
+//   {
+//     url: './assets/images/Photo-gallery-6.jpg',
+//   },
+// ];
 
+let images = [];
+const apiKey =
+  'live_kT9IUM097S8mkNWl8OxZ4onGT2LTWhVh78WXjCTZE5mRBR5Fu1LCjw9i1zDlDThR';
+
+const getPetImgs = () => {
+  axios
+    .get('https://api.thedogapi.com/v1/images/search?limit=10')
+    .then((response) => {
+      console.log(response.data);
+      images = response.data;
+      showGallery();
+    })
+    .catch((err) => {
+      console.log('Error happened during fetching!', err);
+    });
+};
+getPetImgs();
+
+//----------------------------
 const showGallery = () => {
-	images.forEach((image) => {
-		// create
-		createImageContainer(image.url);
-	});
+  images.forEach((image) => {
+    // create
+    createImageContainer(image.url);
+  });
 };
 
 const createImageContainer = (imgUrl) => {
-	/**
+  /**
 	 * 
 	 * <div class="photo-gallery__item">
 						<img
@@ -54,16 +73,16 @@ const createImageContainer = (imgUrl) => {
 					</div>
 	 * 
 	 */
-	let parentEl = document.querySelector(".photo-gallery");
+  let parentEl = document.querySelector('.photo-gallery');
 
-	let photoGalleryItem = document.createElement("div");
-	photoGalleryItem.classList.add("photo-gallery__item");
+  let photoGalleryItem = document.createElement('div');
+  photoGalleryItem.classList.add('photo-gallery__item');
 
-	let img = document.createElement("img");
-	img.classList.add("photo-gallery__image");
-	img.src = imgUrl;
-	photoGalleryItem.appendChild(img);
-	parentEl.appendChild(photoGalleryItem);
+  let img = document.createElement('img');
+  img.classList.add('photo-gallery__image');
+  img.src = imgUrl;
+  photoGalleryItem.appendChild(img);
+  parentEl.appendChild(photoGalleryItem);
 };
 
 // Onload show gallery
